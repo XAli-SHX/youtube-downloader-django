90aa0e9d799ed7d787bc7da77851b25cd198e4a9
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-05-13 22:20:13 +0330

    Add status

--- core/logger/models.py
+++ core/logger/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from enum import Enum
 
 
 class Logger(models.Model):
+    class Status(Enum):
+        SUCCESS = 'success'
+        ERROR = 'error'
+
     name = models.CharField(max_length=255)
     path = models.TextField()
     link = models.TextField()
+    status = models.CharField(max_length=255)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
