8c2be3702621bc93eca37c7880c237e4b755ef02
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-05-13 20:00:26 +0330

    Fix timestamp bug and apply filename

--- core/downloader/views.py
+++ core/downloader/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
 import pytube
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def download(request):
     if request.method == 'POST':
         link = request.POST['link']
         video = pytube.YouTube(link)
         stream = video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-        filename = video.title + timezone.now().timestamp() + '.mp4'
-        stream.download()
-        # stream.download('./media/', filename=filename)
+        filename = video.title + str(timezone.now().timestamp()) + '.mp4'
+        # stream.download()
+        stream.download('../media/', filename=filename)
         return render(request, 'index.html')
     return render(request, 'index.html')
