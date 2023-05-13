c5f2c63372bea941cef8fc49bc4ca1c2195adbbe
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-05-13 19:41:53 +0330

    Create view function to respond to download

--- core/downloader/views.py
+++ core/downloader/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import pytube
+from django.utils import timezone
 
-# Create your views here.
+
+def download(request):
+    if request.method == 'POST':
+        link = request.POST['link']
+        video = pytube.YouTube(link)
+        stream = video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+        filename = video.title + timezone.now().timestamp() + '.mp4'
+        stream.download()
+        # stream.download('./media/', filename=filename)
+        return render(request, 'index.html')
+    return render(request, 'index.html')
