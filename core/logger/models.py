3f7fbc023da003bbcf5c41f01d444f3286c95ba8
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-05-13 18:54:04 +0330

    Create Logger model

--- core/logger/models.py
+++ core/logger/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Logger(models.Model):
+    name = models.CharField(max_length=255)
+    path = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
