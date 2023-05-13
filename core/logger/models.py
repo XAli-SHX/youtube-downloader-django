907570ad2b322cdc63877d98b03f81294de794cb
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-05-13 22:15:59 +0330

    Add link to Logger

--- core/logger/models.py
+++ core/logger/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Logger(models.Model):
     name = models.CharField(max_length=255)
     path = models.TextField()
+    link = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
