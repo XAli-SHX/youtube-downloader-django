9a192f51c3b364d7d6efa9c0bbae0a65da7ae3ea
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-05-13 22:23:41 +0330

    Add logger to downloader

--- core/downloader/views.py
+++ core/downloader/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 import pytube
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
+from logger import Logger
 
 
 @csrf_exempt
@@ -11,7 +12,16 @@ def download(request):
         video = pytube.YouTube(link)
         stream = video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
         filename = video.title + str(timezone.now().timestamp()) + '.mp4'
+        path = '../media/' + filename
         # stream.download()
+        logger = Logger.objects.create(
+            name=filename,
+            path=path,
+            link=link,
+            status=Logger.Status.ERROR,
+        )
         stream.download('../media/', filename=filename)
+        logger.status = Logger.Status.SUCCESS
+        logger.save()
         return render(request, 'index.html')
     return render(request, 'index.html')
